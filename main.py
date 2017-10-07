3a6261e2c789397e6d2f43e2d5a56efa1039f442
tyreest19 <tyreeostevenson@gmail.com>
2017-10-07 11:48:54 -0700

    Created layout of main

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def home():
+    return "Welcome to CarNow"
+
+
+@app.route("/search")
+def searchCar():
+    """Page will allow users to search for a car"""
+
+
+
+def presenetCar():
+    """This page will load all the default care info and be basic layout for cars."""
