ea38faa587cde331daf2a36bb7ed70715946700b
DarianNwankwo <dnwankwo404@gmail.com>
2017-10-07 16:39:24 -0700

    Updates

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+app.config['DEBUG'] = False
 
 
 @app.route("/")
 def home():
-    return "Welcome to CarNow"
+    # return "Welcome to CarNow"
+    return render_template("index.html")
 
 
 @app.route("/search")
@@ -12,6 +14,8 @@ def searchCar():
     """Page will allow users to search for a car"""
 
 
-
 def presenetCar():
     """This page will load all the default care info and be basic layout for cars."""
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
