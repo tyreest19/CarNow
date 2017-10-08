053641bef6f0840445284d932d5f683b001747bb
tyreest19 <tyreeostevenson@gmail.com>
2017-10-08 03:19:46 -0700

    added twillo

---
+++ utils.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from Model import database
+from twilio.rest import Client
+
+
+def messageSalesRep(customer_name, customer_email, customer_message):
+    print(customer_name)
+    print(customer_message)
+# Your Account Sid and Auth Token from twilio.com/user/account
+    account_sid = "AC1c7bbdb77eb80d1916a3abb03c99e150"
+    auth_token = "6f542f28084b112c3074ef070241e028"
+    client = Client(account_sid, auth_token)
+        #if customer_email == None:
+
+    if not customer_email:
+        message = client.messages.create(
+        to="+13142655522",
+        from_="+14159153599",
+        body="Name: " + customer_name + "\n"
+        + "Message: " + customer_message)
+
+    else:
+        message = client.messages.create(
+        to="+13142655522",
+        from_="+14159153599",
+        body="Name: " + customer_name + "\n" + "Email: " + customer_email + "\n"
+        + "Message: " + customer_message)
+
+
+    print(message.sid)
